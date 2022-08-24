c36234ae9a2136e82785671e6ccd2ad06b02dffb
aollerv <alejandro.oller@suscrip.com>
2022-08-24 11:15:24 -0400

    Charts working

--- src/containers/ResultDashboard.js
+++ src/containers/ResultDashboard.js
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef} from 'react';
+import Chart from 'chart.js/auto';
 
 export default function ResultDashboard() {
 
@@ -9,6 +10,37 @@ export default function ResultDashboard() {
     "questionSubDimensions" : [],
     "scores" : []
   }
+
+  const chartData = {
+    labels: [],
+    datasets: [{
+      label: "QS2",
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(255, 99, 132)',
+      data: [],
+    }]
+  };
+
+  const chartData2 = {
+    labels: [],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [],
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(255, 99, 132)'
+    }]
+  }
+
 
   function fetchResults(){
     const questionnaireCode = resultCodeQuestionnaire.current.value
@@ -23,15 +55,53 @@ export default function ResultDashboard() {
     })
     .then(response => response.json())
     .then(response => {
-        console.log(response)
-        results.questionnaireCode = response.questionnaireCode
-        results.questionSubDimensions = response.subDimensions
-        results.scores = response.scores
-        console.log(results)
+      
+      results.questionnaireCode = response.questionnaireCode
+      results.questionSubDimensions = response.subDimensions
+      results.scores = response.scores
+
+      chartData2.labels = results.questionSubDimensions
+      chartData2.datasets[0].label = results.questionnaireCode
+      chartData2.datasets[0].data = results.scores
+      
+      console.log(chartData2)
+      
+      var options = {
+        responsive: true,
+        maintainAspectRadio: true,
+        legend: {
+          position: 'top',
+          },
+        title: {
+            display: true,
+            text: 'Radar Chart'
+            },
+            scales :{
+              r: {
+                max:5,
+                min:0
+              }
+          },
+         };
+      
+      const config = {
+        type: 'radar',
+        data: chartData2,
+        options: options
+      };
+
+      config.data = chartData2
+      console.log(chartData2)
+      config.options = options
+
+      const spiderChart = new Chart(
+        document.getElementById('chart').getContext('2d'),
+        config
+      )
+      console.log(config)
     })
     resultCodeQuestionnaire.current.value = null
   }
-
 
 
   return (
@@ -40,7 +110,10 @@ export default function ResultDashboard() {
         <div className='questionnaireFetcher'>
             <input type="text" ref={resultCodeQuestionnaire}/> 
             <input type="submit" onClick={fetchResults}/> 
-      </div>
+        </div>
+        <div id='chartContainer'>
+            <canvas id='chart' width='100' height='100' ></canvas>
+        </div>
     </div>
   )
 }
